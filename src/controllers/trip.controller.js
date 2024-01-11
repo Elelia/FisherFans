07de73b891674841c89faf1f26c81128385a221b
Elelia <llucas@ensup.eu>
2024-01-11 14:42:39 +0100

    update trips

--- src/controllers/trip.controller.js
+++ src/controllers/trip.controller.js
@@ -23,7 +23,23 @@ async function getAllTrips(req, res) {
 
 // Create one trip
 async function createTrip(req, res) {
-	
+  try {
+    const result = await tripModel.createTrip(req.body);
+    console.log(result);
+    if(result) {
+      res.status(201).json({
+        success: true,
+        message: 'Trip created successfully',
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: 'Trip could not be created',
+      });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 }
 
 // Delete one trip
--- src/models/trip.model.js
+++ src/models/trip.model.js
@@ -12,7 +12,32 @@ async function getAllTrips() {
 
 // Insert one trip
 async function createTrip(body) {
+  try {
+    const query = `INSERT INTO trips (title, informations, trip_type, price_type, start_date, end_date, start_hour, end_hour, passenger_number, price, creator_id, boat_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
     
+    const [results] = await db.promise().execute(query, [
+      body.title,
+      body.informations,
+      body.trip_type,
+      body.price_type,
+      body.start_date,
+      body.end_date,
+      body.start_hour,
+      body.end_hour,
+      body.passenger_number,
+      body.price,
+      body.creator_id,
+      body.boat_id
+    ]);
+
+    if (results.affectedRows === 1) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    throw error;
+  }
 }
 
 // Delete one trip
